1e26f7ffd3cca427a6de9d5cc09e1d312c6b1484
Charlie <bringking@gmail.com>
2015-03-28 10:41:04 -0700

    added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
-var gulp     = require('gulp'),
-    gwebpack = require('gulp-webpack');
-
+/**
+ * The idiomatic ReactJS application compiles JSX as a build step, and doesn't use the in-browser JSX transformer.
+ * This repo uses a very basic gulp + webpack + babel build to write React with ES6, and build it for the browser
+ * @type {Gulp|exports}
+ */
+var gulp       = require('gulp')
+    , gwebpack = require('gulp-webpack');
+/**
+ * A simple task to copy our HTML file and images to the dist directory
+ */
 gulp.task('copy', function() {
-    return gulp.src("./app/index.html").pipe(gulp.dest("dist/"));
+    var html = gulp.src("./app/index.html").pipe(gulp.dest("dist/"))
+        , images = gulp.src("./app/images/**/*.*").pipe(gulp.dest("dist/images"));
 });
+/**
+ * The main step is 'pack' this step takes our 'client.js' file, and builds it using Webpack, the babel-loader plugin to
+ * transpile ES6 and outputs it to the dist directory
+ */
 gulp.task('pack', function() {
     return gulp.src('./app/client.js')
         .pipe(gwebpack({
@@ -24,5 +36,11 @@ gulp.task('pack', function() {
         .pipe(gulp.dest('dist/'));
 });
 
+/**
+ * The default task is build
+ */
 gulp.task("default", ["build"]);
+/**
+ * Define our build task
+ */
 gulp.task("build", ["copy", "pack"]);
