33d180f99bb8a0ff5246d9a08756e018075846d0
Charlie <bringking@gmail.com>
2015-03-29 10:23:14 -0700

    moved into arrow

---
+++ app/repo_list/trending_up_arrow.jsx
@@ -0,0 +1,22 @@
+'use strict';
+let React = require('react');
+let FluxibleMixin = require('fluxible').FluxibleMixin;
+
+let TrendingUpArrow = React.createClass({
+    mixins: [FluxibleMixin],
+    getInitialState() {
+        return {};
+    },
+    render() {
+
+
+        //render content
+        return (
+            <div>
+
+            </div>
+        );
+    }
+});
+
+module.exports = TrendingUpArrow;
